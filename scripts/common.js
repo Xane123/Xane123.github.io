391d35beba07c5265f84d5b6e22ac3dc7d7faa9f
Xane Myers <xanem1@gmail.com>
2017-10-06 22:37:15 -0400

    Added test function to see if new JS works.
    To be precise, add "?testing=yes" to the end of the URL of the homepage (index.html).
    
    Also, yes, the code came from a scam website! It seems like it could come in handy for a URL which only checks parameters then redirects you accordingly...it could work.

--- scripts/common.js
+++ scripts/common.js
@@ -1,2 +1,7 @@
 function SetPageTitle()
 {var element = document.getElementById('headerid');element.innerHTML = element.innerText || element.textContent;document.title = element.innerHTML;}
+
+//From the Official Microsoft Support scam website, getURLParameter()!
+//This script is so you can get fields out of the URL to put in variables. UPDATED VERSION THAT ACTUALLY CHECKS FULL PARAMETER NAME BEGIN OF ? OR &amp;
+function getURLParameter(name) {
+	return decodeURIComponent((new RegExp('[?|&amp;]' + name + '=' + '([^&amp;;]+?)(&amp;|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;}
