a338201e7735eebf536f34b7113c30e2ebb3c8b8
Xane Myers <xanem1@gmail.com>
2017-10-07 14:39:28 -0400

    Doing more tests with JS and trying to pass more audits in Chrome.

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,19 @@
 
 
-function SetupPage()
-{var sitename = 'Xane.com';var text_header = document.getElementById('headerid');var text_logo = document.getElementById('logotextid');text_header.textContent = document.title;text_logo.textContent = sitename;document.title = sitename + ' - ' + document.title;}
+function SetupPage() {
+	var sitename = 'Xane.com';
+	var text_header = document.getElementById('headerid');
+	var text_logo = document.getElementById('logotextid');
+	var text_content = document.getElementById('contentid');
+	
+	text_header.textContent = document.title;
+	text_logo.textContent = sitename;
+	document.title = sitename + ' - ' + document.title;
+	
+	var temp_content = text_content.internalHTML;
+	temp_content.replace(/SiteName/g, sitename);
+	text_content.internalHTML = temp_content;
+}
 //{var element = document.getElementById('headerid');element.innerHTML = element.innerText || element.textContent;document.title = element.innerHTML;}
 
 //From the Official Microsoft Support scam website, getURLParameter()!
