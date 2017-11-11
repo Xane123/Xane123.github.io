d92aeb3a66d21104f71c9a391cafd606a26f3e94
Xane Myers <xanem1@gmail.com>
2017-11-10 22:31:32 -0500

    Added different capitalize variants for censorship.

--- xprivaterefs/fixpages.js
+++ xprivaterefs/fixpages.js
@@ -8,8 +8,14 @@ temp_content = temp_content.replace(/â€/g, "”");
 
 //Next, fix some other things.
 temp_content = temp_content.replace(/fuck/g, "fart");
+temp_content = temp_content.replace(/Fuck/g, "Fart");
+temp_content = temp_content.replace(/FUCK/g, "EPIC FART");
 temp_content = temp_content.replace(/shit/g, "poop");
+temp_content = temp_content.replace(/Shit/g, "Poop");
+temp_content = temp_content.replace(/SHIT/g, "POOP EXPLOSION");
 temp_content = temp_content.replace(/piss/g, "pee");
+temp_content = temp_content.replace(/Piss/g, "Pee");
+temp_content = temp_content.replace(/piss/g, "DA YELLOW STUFF");
 temp_content = temp_content.replace(/Sanic/g, "Sonikku~!");
 temp_content = temp_content.replace(/SANIC/g, "SONIC-A~!");
 
