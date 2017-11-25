d1f950b3249ae3286df0cf64ed9beb331c8981ac
Xane Myers <xanem1@gmail.com>
2017-11-25 10:58:21 -0500

    Added another word to my censor filter to fix the 'poopty' oddity.

--- xprivaterefs/fixpages.js
+++ xprivaterefs/fixpages.js
@@ -1,3 +1,4 @@
+//This makes the internet a bit less frustrating with language.
 var bodaey = document.body.innerHTML;
 var temp_content = bodaey;
 
@@ -8,10 +9,13 @@ temp_content = temp_content.replace(/â€/g, "”");
 
 //Next, fix some other things.
 temp_content = temp_content.replace(/fuck/g, "fart");
-temp_content = temp_content.replace(/Fuck/g, "Fart");
+temp_content = temp_content.replace(/Fuck/g, "The One Named 'Fart'");
 temp_content = temp_content.replace(/FUCK/g, "EPIC FART");
+temp_content = temp_content.replace(/shitty/g, "poop-filled");
+temp_content = temp_content.replace(/Shitty/g, "but it's named Poop Will Fill");
+temp_content = temp_content.replace(/SHITTY/g, "POOP-FILLED EXPLOSION");
 temp_content = temp_content.replace(/shit/g, "poop");
-temp_content = temp_content.replace(/Shit/g, "Poop");
+temp_content = temp_content.replace(/Shit/g, "The Poop");
 temp_content = temp_content.replace(/SHIT/g, "POOP EXPLOSION");
 temp_content = temp_content.replace(/pissed/g, "pee sprayed at 'em then washed");
 temp_content = temp_content.replace(/Pissed/g, "...Well, y'see, pee sprayed at 'em then they'd wash it");
